afefa0518e17131ad0185f228693865b989f5225
Narges <s0ltanian3099@gmail.com>
2025-04-04 22:50:03 +0330

    تمرین پایتون 2

---
+++ 2.py
@@ -0,0 +1,9 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1 = float(input("لطفاً عدد اول را وارد کنید: "))
+... num2 = float(input("لطفاً عدد دوم را وارد کنید: "))
+... num3 = float(input("لطفاً عدد سوم را وارد کنید: "))
+... num4 = float(input("لطفاً عدد چهارم را وارد کنید: "))
+...     
+... largest = max(num1, num2, num3, num4)
+...    
